7b30b768b3f8df12f4c70f3d9f030394b071c5b8
ardee222 <ardeeche@gmail.com>
2023-05-26 23:31:05 +0800

    xc

--- song/src/App.svelte
+++ song/src/App.svelte
@@ -5,6 +5,7 @@
 <main class="w-max-md w-screen h-screen bg-red-100">
     <video width="320" height="240" controls>
         <source src="./src/song-list/Cupid - FIFTY FIFTY.mp4" type="video/mp4">
+        <track kind="captions">
     </video>
 </main>
 
