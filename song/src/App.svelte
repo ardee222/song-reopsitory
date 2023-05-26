3f131f05e18cd76a3e525daba788a3d9a7ab6b16
ardee222 <ardeeche@gmail.com>
2023-05-26 23:35:49 +0800

    Update App.svelte

--- song/src/App.svelte
+++ song/src/App.svelte
@@ -4,6 +4,7 @@
 
 <main class="w-max-md w-screen h-screen bg-red-100">
     <video width="320" height="240" controls>
+        <source src="https://rr5---sn-juh-h4hz.googlevideo.com/videoplayback?expire=1685136872&ei=iNFwZNm8I9ql2roPk-qUyAM&ip=27.109.210.7&id=o-ADSEDZ0X-3ExZ70qzKUZtSOlgIoQV6psfpHeN8xob6oB&itag=18&source=youtube&requiressl=yes&mh=rF&mm=31%2C29&mn=sn-juh-h4hz%2Csn-i3b7knzs&ms=au%2Crdu&mv=m&mvi=5&pl=20&initcwndbps=1118750&vprv=1&svpuc=1&mime=video%2Fmp4&ns=6z-HRn5TAc7ZVyODgpM7i90N&gir=yes&clen=14544260&ratebypass=yes&dur=182.186&lmt=1684671549630311&mt=1685114929&fvip=4&fexp=24007246%2C24363392%2C51000014&beids=24350018&c=WEB&txp=4530434&n=Uwft1wqgJAeVouwz7e&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Csvpuc%2Cmime%2Cns%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIhAPJLa3StLS_9gibd7jhODGHax0a93IwmG-zDJDZzcEQWAiBNK1juJUgibKxaOIBpnPPwnOlx-Aeha8fxzEJX0OnHXA%3D%3D&sig=AOq0QJ8wRAIgVvVqrGWRlrVTNwxslhPVHs7D14UZn8Hxah5uiQQPZXkCIAfUg9DOj_ukDTLzadAqebl7R0eRG9j0ufyzSvLwVdeQ" type="video/webm">
         <source src="./src/song-list/Cupid - FIFTY FIFTY.mp4" type="video/mp4">
         <track kind="captions">
     </video>
