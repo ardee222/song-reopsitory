855ab01a49124a0b0ec28a0a58fa11b76d3549e9
ardee222 <ardeeche@gmail.com>
2023-05-26 23:03:14 +0800

    .

--- song/src/App.svelte
+++ song/src/App.svelte
@@ -2,10 +2,14 @@
 
 </script>
 
-<main>
-
+<main class="w-max-md w-screen h-screen bg-red-100">
+    <audio width="320" height="240" controls>
+        <source src="./src/song-list/Cupid - FIFTY FIFTY.mp4" type="audio/mp3">
+    </audio>
 </main>
 
 <style>
-  
+*{
+    box-sizing: border-box;
+}
 </style>
